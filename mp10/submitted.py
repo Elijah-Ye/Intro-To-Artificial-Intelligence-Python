839a6b175cf1643fb3150ce5dd53fd63601fa1c5
Elijah-Ye <elijah.gaohan.ye@gmail.com>
2023-04-17 12:46:54 -0500

    Adding RL into Repo

--- mp10/submitted.py
+++ mp10/submitted.py
@@ -17,7 +17,79 @@ def compute_transition_matrix(model):
     Output:
     P - An M x N x 4 x M x N numpy array. P[r, c, a, r', c'] is the probability that the agent will move from cell (r, c) to (r', c') if it takes action a, where a is 0 (left), 1 (up), 2 (right), or 3 (down).
     '''
-    raise RuntimeError("You need to write this part!")
+    P = np.zeros([model.M, model.N, 4, model.M, model.N])
+    for m in range(model.M):
+        for n in range(model.N):
+        
+            if(model.T[m, n] == True):
+                P[m, n, :, :, :] = 0
+            else:
+            # left
+                if n-1 >= 0 and model.W[m, n-1] == False:
+                    P[m, n, 0, m, n-1] = model.D[m, n, 0]
+                elif (n-1 >= 0 and model.W[m, n-1] == True) or n - 1 < 0:
+                    P[m, n, 0, m, n] += model.D[m, n, 0]
+                # down
+                if m + 1 < model.M and model.W[m + 1, n] == False:
+                    P[m, n, 0, m + 1, n] = model.D[m, n, 1]
+                elif (m + 1 < model.M and model.W[m + 1, n] == True) or m + 1 >= model.M:
+                    P[m, n, 0, m, n] += model.D[m, n, 1]
+                # up
+                if m - 1 >= 0 and model.W[m -1, n] == False:
+                    P[m, n, 0, m - 1, n] = model.D[m, n, 2]
+                elif (m - 1 >= 0 and model.W[m -1, n] == True) or m - 1 < 0:
+                    P[m, n, 0, m, n] += model.D[m, n, 2]
+            
+            # up
+                if m - 1 >= 0 and model.W[m -1, n] == False:
+                    P[m, n, 1, m - 1, n] = model.D[m, n, 0]
+                elif (m - 1 >= 0 and model.W[m -1, n] == True) or m - 1 < 0:
+                    P[m, n, 1, m, n] = model.D[m, n, 0]
+                # left
+                if n-1 >= 0 and model.W[m, n-1] == False:
+                    P[m, n, 1, m, n-1] = model.D[m, n, 1]
+                elif (n-1 >= 0 and model.W[m, n-1] == True) or n - 1 < 0:
+                    P[m, n, 1, m, n] += model.D[m, n, 1]
+                # right
+                if n+1 < model.N and model.W[m, n+1] == False:
+                    P[m, n, 1, m, n+1] = model.D[m, n, 2]
+                elif (n+1 < model.N and model.W[m, n+1] == True) or n + 1 >= model.N:
+                    P[m, n, 1, m, n] += model.D[m, n, 2]
+                    
+            # right
+                if n+1 < model.N and model.W[m, n+1] == False:
+                    P[m, n, 2, m, n+1] = model.D[m, n, 0]
+                elif (n+1 < model.N and model.W[m, n+1] == True) or n + 1 >= model.N:
+                    P[m, n, 2, m, n] += model.D[m, n, 0]
+                # up
+                if m - 1 >= 0 and model.W[m - 1, n] == False:
+                    P[m, n, 2, m - 1, n] = model.D[m, n, 1]
+                elif (m - 1 >= 0 and model.W[m -1, n] == True) or m - 1 < 0:
+                    P[m, n, 2, m, n] += model.D[m, n, 1]
+                # down
+                if m + 1 < model.M and model.W[m + 1, n] == False:
+                    P[m, n, 2, m + 1, n] = model.D[m, n, 2]
+                elif (m + 1 < model.M and model.W[m + 1, n] == True) or m + 1 >= model.M:
+                    P[m, n, 2, m, n] += model.D[m, n, 2]
+            
+            #down
+                if m + 1 < model.M and model.W[m + 1, n] == False:
+                    P[m, n, 3, m + 1, n] = model.D[m, n, 0]
+                elif (m + 1 < model.M and model.W[m + 1, n] == True) or m + 1 >= model.M:
+                    P[m, n, 3, m, n] += model.D[m, n, 0]
+                # right
+                if n+1 < model.N and model.W[m, n+1] == False:
+                    P[m, n, 3, m, n+1] = model.D[m, n, 1]
+                elif (n+1 < model.N and model.W[m, n+1] == True) or n + 1 >= model.N:
+                    P[m, n, 3, m, n] += model.D[m, n, 1]
+                # left
+                if n-1 >= 0 and model.W[m, n-1] == False:
+                    P[m, n, 3, m, n-1] = model.D[m, n, 2]
+                elif (n-1 >= 0 and model.W[m, n-1] == True) or n - 1 < 0:
+                    P[m, n, 3, m, n] += model.D[m, n, 2] 
+    return P
+                    
+    #raise RuntimeError("You need to write this part!")
 
 def update_utility(model, P, U_current):
     '''
@@ -29,7 +101,20 @@ def update_utility(model, P, U_current):
     Output:
     U_next - The updated utility function, which is an M x N array
     '''
-    raise RuntimeError("You need to write this part!")
+    U_next = np.zeros([model.M, model.N])
+    for m in range(model.M):
+        for n in range(model.N):
+            if model.T[m,n] == True:
+                U_next[m,n] = model.R[m, n]
+            else:
+                U_next[m][n] = model.R[m, n] + model.gamma * max(
+                    (P[m, n, 0, :, :]*U_current).sum(), 
+                    (P[m, n, 1, :, :]*U_current).sum(),
+                    (P[m, n, 2, :, :]*U_current).sum(),
+                    (P[m, n, 3, :, :]*U_current).sum())
+    
+    return U_next
+    #raise RuntimeError("You need to write this part!")
 
 def value_iteration(model):
     '''
@@ -39,7 +124,13 @@ def value_iteration(model):
     Output:
     U - The utility function, which is an M x N array
     '''
-    raise RuntimeError("You need to write this part!")
+    U = np.zeros([model.M, model.N])
+    P = compute_transition_matrix(model)
+    for i in range(100):
+        U = update_utility(model, P, U)
+        
+    return U
+    # raise RuntimeError("You need to write this part!")
 
 if __name__ == "__main__":
     import utils
